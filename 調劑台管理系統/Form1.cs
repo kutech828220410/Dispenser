1069f7fc077d91fd2cd07a6a031aa36bc4cde7e7
Evan <Hson_Evan@outlook.com>
2022-12-14 14:56:57 +0800

    1.更新Ver1.0.14

--- 調劑台管理系統/Form1.cs
+++ 調劑台管理系統/Form1.cs
@@ -18,8 +18,8 @@ using System.Text.Json.Serialization;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.13.0")]
-[assembly: AssemblyFileVersion("1.0.13.0")]
+[assembly: AssemblyVersion("1.0.14.0")]
+[assembly: AssemblyFileVersion("1.0.14.0")]
 namespace 調劑台管理系統
 {
 
@@ -240,15 +240,15 @@ namespace 調劑台管理系統
                 LoadMyConfig();
                 LoadFtpConfig();
 
-                dBConfigClass.DB_Basic.DataBaseName = "test01";
-                dBConfigClass.DB_Basic.IP = "localhost";
-                dBConfigClass.DB_Basic.UserName = "root";
-                dBConfigClass.DB_Basic.Password = "user82822040";
-                dBConfigClass.DB_Basic.Port = 3306;
+                //dBConfigClass.DB_Basic.DataBaseName = "test01";
+                //dBConfigClass.DB_Basic.IP = "localhost";
+                //dBConfigClass.DB_Basic.UserName = "root";
+                //dBConfigClass.DB_Basic.Password = "user82822040";
+                //dBConfigClass.DB_Basic.Port = 3306;
 
-                dBConfigClass.DB_person_page = dBConfigClass.DB_Basic;
-                dBConfigClass.DB_Medicine_Cloud = dBConfigClass.DB_Basic;
-                dBConfigClass.DB_order_list = dBConfigClass.DB_Basic;
+                //dBConfigClass.DB_person_page = dBConfigClass.DB_Basic;
+                //dBConfigClass.DB_Medicine_Cloud = dBConfigClass.DB_Basic;
+                //dBConfigClass.DB_order_list = dBConfigClass.DB_Basic;
 
                 this.stopwatch.Start();            
                 this.Text += "Ver" + this.ProductVersion;
